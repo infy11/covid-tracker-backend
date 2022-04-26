98f2a0fd18db758a6c680b2153cb6e14a0fc0a9a
varnit <varnitgoyal95@gmail.com>
2022-04-26 14:03:50 +0530

    update cors ext

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'jade');
 let cache = apicache.middleware
 
 app.use(logger('dev'));
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
